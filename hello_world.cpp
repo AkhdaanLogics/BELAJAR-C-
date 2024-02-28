1a4a4c10d04bc3117d930b3cf64c796dbd9ccec1
Akhdaan <53220707+AkhdaanLogics@users.noreply.github.com>
2024-02-28 16:49:12 +0700

    Create hello_world.cpp
    Materi 1

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!";
+}
+
+/*Penjelasan :
+1.  #include <...> biasa disebut dengan library, banyak library yang tersedia dan memiliki fungsi yang berbeda - beda
+2.  using namespace std merupakan suatu fungsi yang digunakan untuk mempersingkat penggunaan code, yang mana dapat menambah efisiensi dalam coding
+3.  int main() merupakan fungsi yang harus ada di dalam c++ diibaratkan sebagai otak dari program
+*/
