9abc3d4095bed8adc5f0c5c38cffa0a0239b312e
Akhdaan <53220707+AkhdaanLogics@users.noreply.github.com>
2024-02-28 17:17:40 +0700

    Create void.cpp

---
+++ void.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void menu()
+{
+  cout << "Hello World!"
+}
+
+int main()
+{
+  menu();
+}
+
+/*
+1.  void merupakan tipe data tanpa kembalian
+2.  void dapat digunakan untuk meringkas code sehingga terlihat lebih
+    rapi
+3.  cara memanggil void adalah dengan menuliskan nama void ke dalam main dengan
+    format nama_void();
+*/
