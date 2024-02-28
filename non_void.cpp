b648112cdc77178e7a4113ef5b3fd28ff603bd9b
Akhdaan <53220707+AkhdaanLogics@users.noreply.github.com>
2024-02-28 17:23:54 +0700

    non_void.cpp

---
+++ non_void.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+string data;
+
+int main()
+{
+  cout << "Masukkan nama anda : ";
+  cin >> data;
+  cout << "Nama anda adalah " << data;
+}
+
+/*
+1.  non void merupakan kebalikan dari void, non void dapat mengembalikan nilai
+2.  perbedaan paling mencolok antara void dan non void apabila void pasti
+    ada kata void di depan variabel
+*/
